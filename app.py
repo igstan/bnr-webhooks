f47318d4871a619726db78c7e8f8aa07eb5bbee1
Alex Morega <alex@grep.ro>
2012-08-21 20:52:39 +0300

    register a database

--- app.py
+++ app.py
@@ -17,9 +17,25 @@ def get_date(xml):
     return pattern.search(xml).group('date')
 
 
+def setup_database(app):
+    from werkzeug.local import LocalProxy
+    from flask.ext.htables import HTables
+
+    HTables(app)
+
+    def _get_session():
+        return flask.current_app.extensions['htables'].session
+
+    dbs = app.dbs = LocalProxy(_get_session)
+
+    with app.app_context():
+        dbs['subscriber'].create_table()
+
+
 def create_app():
     import clients
     app = flask.Flask(__name__)
+    setup_database(app)
     app.register_blueprint(clients.client_views)
     return app
 
