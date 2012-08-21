75da1615632a78b6304cc7bc26e0d3c9d0c8da88
Alex Morega <alex@grep.ro>
2012-08-21 20:18:28 +0300

    start a flask app

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python
 import requests
+import flask
 
 
 BNR_URL = 'http://bnr.ro/nbrfxrates.xml'
@@ -7,3 +9,13 @@ BNR_URL = 'http://bnr.ro/nbrfxrates.xml'
 def get_bnr():
     resp = requests.get(BNR_URL)
     return resp.text
+
+
+def create_app():
+    app = flask.Flask(__name__)
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
