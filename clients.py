3ba136e476a60d311dbf868d0f76bf05daf5f5dd
Alex Morega <alex@grep.ro>
2012-08-21 20:23:57 +0300

    actually homepage redirects to /hooks

--- clients.py
+++ clients.py
@@ -6,4 +6,9 @@ client_views = flask.Blueprint('client', __name__)
 
 @client_views.route('/')
 def homepage():
-    return flask.render_template('homepage.html')
+    return flask.redirect(flask.url_for('.hooks'))
+
+
+@client_views.route('/hooks')
+def hooks():
+    return flask.render_template('hooks.html')
