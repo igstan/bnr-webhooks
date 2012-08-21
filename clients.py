16ff9460758b756b4bc6a5a04f8a82e7bdeb1b0f
Alex Morega <alex@grep.ro>
2012-08-21 20:35:52 +0300

    save subscriber on the app

--- clients.py
+++ clients.py
@@ -4,6 +4,15 @@ import flask
 client_views = flask.Blueprint('client', __name__)
 
 
+def get_subscribers():
+    return flask.current_app.extensions['bnrhooks-subscribers']
+
+
+@client_views.record
+def initialize_subscribers(state):
+    state.app.extensions['bnrhooks-subscribers'] = []
+
+
 @client_views.route('/')
 def homepage():
     return flask.redirect(flask.url_for('.hooks'))
@@ -12,5 +21,6 @@ def homepage():
 @client_views.route('/hooks', methods=['GET', 'POST'])
 def hooks():
     if flask.request.method == 'POST':
+        get_subscribers().append(flask.request.form['uri'])
         return ('subscribed', 201)
     return flask.render_template('hooks.html')
--- tests.py
+++ tests.py
@@ -16,8 +16,15 @@ class ClientTest(FlaskTestCase):
     def setUp(self):
         import clients
         self.app.register_blueprint(clients.client_views)
+        self.client = self.app.test_client()
 
     def test_subscribe_returns_201(self):
-        client = self.app.test_client()
-        resp = client.post('/hooks', data={'uri': 'http://example.com'})
+        resp = self.client.post('/hooks', data={'uri': 'http://example.com'})
         self.assertEqual(resp.status_code, 201)
+
+    def test_subscribe_url_is_saved(self):
+        import clients
+        URI = 'http://example.com'
+        resp = self.client.post('/hooks', data={'uri': URI})
+        with self.app.app_context():
+            self.assertEqual(clients.get_subscribers(), [URI])
